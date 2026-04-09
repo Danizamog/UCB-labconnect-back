5ae65aab38fdd04eade08e6f526db4ca124647b0
Daniel Alejandro Zamorano Gamarra <daniel.zamorano@ucb.edu.bo>
2026-04-09 00:06:46 -0400

    arreglando el cors para prod

--- api-gateway/app/main.py
+++ api-gateway/app/main.py
@@ -12,6 +12,7 @@ origins = settings.cors_allowed_origins
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex=settings.cors_allowed_origin_regex,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
