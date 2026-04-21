9e92937c12fa13f2e728e3f7ddd90f371283e91e
Joh4nx <johan.chura@ucb.edu.bo>
2026-04-21 17:24:35 -0400

    Modificaciones en reservation-service

--- reservation-service/app/application/container.py
+++ reservation-service/app/application/container.py
@@ -16,18 +16,3 @@ tutorial_session_repo = TutorialSessionRepository(_pb_client, lab_reservation_re
 user_penalty_repo = PenaltyStore(_pb_client)
 lab_access_session_repo = LabAccessSessionRepository()
 laboratory_access_repo = LaboratoryAccessRepository(_pb_client)
-#aaaaaaaaaaaaa
-#aaaaaaaaaaaaa
-#aaaaaaaaaaaaa
-#aaaaaaaaaaaaa
-#aaaaaaaaaaaaa
-#aaaaaaaaaaaaa
-#aaaaaaaaaaaaa
-#aaaaaaaaaaaaa
-
-#aaaaaaaaaaaaa
-#aaaaaaaaaaaaa
-#aaaaaaaaaaaaa
-#aaaaaaaaaaaaa
-#aaaaaaaaaaaaa
-#aaaaaaaaaaaaa
